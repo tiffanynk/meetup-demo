f86295fb26d895c49376693b5eb81ee77526dca4
Tiffany Kanjanabout <tiffany.kanjanabout@flatironschool.com>
2021-08-03 12:22:25 -0600

    adds initial styles, structure, and data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+import NewMeetup from "./pages/NewMeetup";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
-
-export default App;
